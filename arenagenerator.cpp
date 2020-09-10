db88a1838849333ef8c3920b4ec009a2c0514e5b
Vladislav Smirnov <v.smirnov@omprussia.ru>
2020-09-10 21:56:37 +0300

    Initial commit.

---
+++ arenagenerator.cpp
@@ -0,0 +1,127 @@
+#include "arenagenerator.h"
+#include <random>
+#include <stdio.h>
+
+ArenaGenerator::ArenaGenerator() {}
+
+ArenaGenerator::~ArenaGenerator() {}
+
+Array ArenaGenerator::generate(
+        const int width,
+        const int height,
+        const int iterations,
+        const int rate,
+        const int mirror_iterations
+)
+{
+    this->width = width;
+    this->height = height;
+    this->iterations = iterations;
+    this->rate = rate;
+    this->mirror_iterations = mirror_iterations;
+
+    Array result;
+    map = new int[width *height];
+    buffer = new int[width *height];
+    
+    generate_random();
+    simulate(iterations);
+    mirror(false);
+    simulate(mirror_iterations);
+    mirror(true);
+    simulate(mirror_iterations);
+    
+    for (int i = 0; i < width * height; i++)
+    {
+        result.append(map[i]);
+    }
+
+    delete map;
+    delete buffer;
+    return result;
+}
+
+void ArenaGenerator::generate_random()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution < > distrib(0, 100);
+    for (int i = 0; i < width * height; i++)
+    {
+        map[i] = distrib(gen) <= rate ? 1 : 0;
+    }
+}
+
+int ArenaGenerator::apply_rules(
+        const int c,
+        const int tl,
+        const int t,
+        const int tr,
+        const int r,
+        const int br,
+        const int b,
+        const int bl,
+        const int l
+)
+{
+    int sum = tl + t + tr + r + br + b + bl + l;
+    if (c == 0 && sum >= 5) return 1;
+    else if (c == 1 && sum <= 3) return 0;
+    return c;
+}
+
+int ArenaGenerator::get_point(const int x, const int y) const
+{
+    if (x < 0 || x >= width || y < 0 || y >= height) return 1;
+    return map[y *width + x];
+}
+
+void ArenaGenerator::simulate(const int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int tl = get_point(x - 1, y - 1);
+                int t = get_point(x, y - 1);
+                int tr = get_point(x + 1, y - 1);
+                int r = get_point(x + 1, y);
+                int br = get_point(x + 1, y + 1);
+                int b = get_point(x, y + 1);
+                int bl = get_point(x - 1, y + 1);
+                int l = get_point(x - 1, y);
+                buffer[(y *width) + x] = apply_rules(map[(y *width) + x], tl, t, tr, r, br, b, bl, l);
+            }
+        }
+        
+        /* Move data back to map from buffer */
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                map[y * width + x] = buffer[y * width + x];
+            }
+        }
+    }
+}
+
+void ArenaGenerator::mirror(bool vertical) {
+    if (vertical) {
+        for (int y = 0; y < height/2; y++) {
+            for (int x = 0; x < width; x++) {
+                map[(height - y - 1) * width + x] = map[y * width + x];
+            }
+        }
+    } else {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width/2; x++) {
+                map[y * width + (width - x - 1)] = buffer[y * width + x];
+            }
+        }
+    }
+}	
+
+void ArenaGenerator::_bind_methods()
+{
+    ClassDB::bind_method(D_METHOD("generate", "width", "height", "iterations", "rate", "mirror_iterations"), &ArenaGenerator::generate);
+}
---
+++ arenagenerator.h
@@ -0,0 +1,49 @@
+#ifndef ARENAGENERATOR_H
+#define ARENAGENERATOR_H
+#include "core/reference.h"
+
+class ArenaGenerator: public Reference
+{
+    GDCLASS(ArenaGenerator, Reference);
+
+private:
+    int *map;
+    int *buffer;
+    int width;
+    int height;
+    int rate;
+    int iterations;
+    int mirror_iterations;
+
+    void generate_random();
+    int apply_rules(
+        const int c,
+        const int tl,
+        const int t,
+        const int tr,
+        const int r,
+        const int br,
+        const int b,
+        const int bl,
+        const int l
+   );
+    void mirror(bool vertical);
+    void simulate(const int n);
+    int get_point(const int x, const int y) const;
+
+protected:
+        static void _bind_methods();
+
+public:
+    Array generate(
+        const int width,
+        const int height,
+        const int iterations,
+        const int rate,
+        const int mirror_iterations
+   );
+    ArenaGenerator();
+    ~ArenaGenerator();
+};
+
+#endif	// ARENAGENERATOR_H
